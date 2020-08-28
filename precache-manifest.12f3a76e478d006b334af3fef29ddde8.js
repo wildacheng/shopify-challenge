39defd7722d389ea753fcfe0ef7b5b9677a1562b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-28 16:20:48 +0000

    Deploy wildacheng/shopify-challenge to github.com/wildacheng/shopify-challenge.git:gh-pages

---
+++ precache-manifest.12f3a76e478d006b334af3fef29ddde8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc87d245164d2dfe7a568f72228bd3d0",
+    "url": "/shopify-challenge/index.html"
+  },
+  {
+    "revision": "439e56a986815b69662d",
+    "url": "/shopify-challenge/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dce0136541d7f1096845",
+    "url": "/shopify-challenge/static/js/2.3e0ff325.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopify-challenge/static/js/2.3e0ff325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "439e56a986815b69662d",
+    "url": "/shopify-challenge/static/js/main.f898706d.chunk.js"
+  },
+  {
+    "revision": "addea9c55d7dae1b4918",
+    "url": "/shopify-challenge/static/js/runtime-main.8732156a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shopify-challenge/static/media/logo.5d5d9eef.svg"
+  }
+]);
